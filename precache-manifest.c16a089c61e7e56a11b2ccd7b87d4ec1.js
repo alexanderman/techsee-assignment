b4d72c486fe222e7434178439ae87b94811455e7
alexanderman <alexander.shnirman@gmail.com>
2020-07-14 16:46:16 +0300

    Updates

---
+++ precache-manifest.c16a089c61e7e56a11b2ccd7b87d4ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1477f2c79d2ea28bc7c4cc6c6e3f0912",
+    "url": "/techsee-assignment/index.html"
+  },
+  {
+    "revision": "7cf48a6feeda62790a4a",
+    "url": "/techsee-assignment/static/css/main.bfa7a389.chunk.css"
+  },
+  {
+    "revision": "64f547954f0dbec5c5f7",
+    "url": "/techsee-assignment/static/js/2.909a9dc8.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/techsee-assignment/static/js/2.909a9dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf48a6feeda62790a4a",
+    "url": "/techsee-assignment/static/js/main.f04b3799.chunk.js"
+  },
+  {
+    "revision": "cfd8f4e446dddd65f03a",
+    "url": "/techsee-assignment/static/js/runtime-main.5f02013b.js"
+  }
+]);
